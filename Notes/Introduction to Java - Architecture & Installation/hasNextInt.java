cf81a2f2195c99638451622acc3318f85076f2a5
Sahitya Gupta <99082422+SahityaGupta25@users.noreply.github.com>
2025-07-23 02:29:23 -0600

    Create hasNextInt.java

---
+++ Notes/Introduction to Java - Architecture & Installation/hasNextInt.java
@@ -0,0 +1,17 @@
+// How to check, that the variable is containing nu ber or not.
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a number");
+        boolean bc = sc.hasNextInt();
+        System.out.println(bc);
+        
+     
+        
+        
+    }
+    
+}
