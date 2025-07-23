34e2199ac2b7feebdc8fe7351dd8f192128da857
Sahitya Gupta <99082422+SahityaGupta25@users.noreply.github.com>
2025-07-23 02:56:31 -0600

    Create percentageByMe.java

---
+++ Notes/Introduction to Java - Architecture & Installation/percentageByMe.java
@@ -0,0 +1,33 @@
+// Difference between next & nextLine in Java
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        Scanner ac = new Scanner(System.in);
+        System.out.println("Enter marks of Subject 1");
+        int a = sc.nextInt();
+        System.out.println("Enter marks of Subject 2");
+        int b = sc.nextInt();
+        System.out.println("Enter marks of Subject 3");
+        int c = sc.nextInt();
+        System.out.println("Enter marks of Subject 4");
+        int d = sc.nextInt();
+        System.out.println("Enter marks of Subject 5");
+        int e = sc.nextInt();
+        System.out.println("Enter marks of Subject 6");
+        int f = sc.nextInt();
+        int percentage = (a + b + c + d + e + f *100) / 6;
+        System.out.println(percentage);
+
+
+            
+
+
+        
+     
+        
+    
+    }
+    
+}
