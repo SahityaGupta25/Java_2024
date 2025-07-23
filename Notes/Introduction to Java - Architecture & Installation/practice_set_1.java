1a446254ee8f105b758b2d87a5bb370c82fa5994
Sahitya Gupta <99082422+SahityaGupta25@users.noreply.github.com>
2025-07-23 03:25:22 -0600

    Create practice_set_1.java

---
+++ Notes/Introduction to Java - Architecture & Installation/practice_set_1.java
@@ -0,0 +1,14 @@
+// Practice Set 1
+
+
+// 3.  Ask name, & greet.
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        System.out.println("Enter your Full Name");
+        Scanner scan = new Scanner(System.in);
+        String str = scan.nextLine();
+        System.out.println("Good Morning! " + str);
+        
+    }
+}
