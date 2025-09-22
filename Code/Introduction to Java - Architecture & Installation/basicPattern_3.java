ef8be432b098815035ae02fd0905f96d70ed57c2
Sahitya Gupta <99082422+SahityaGupta25@users.noreply.github.com>
2025-09-23 04:19:45 +0530

    Create basicPattern_3.java

---
+++ Code/Introduction to Java - Architecture & Installation/basicPattern_3.java
@@ -0,0 +1,27 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+/*
+4321
+4321
+4321
+4321
+*/
+
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int i = 4;
+        while(i>=1){
+            int j = 4;
+            while(j>=1){
+                System.out.print(j);
+                j--;
+            }
+            System.out.println(" ");
+            i--;
+        }
+        
+    }
+}
