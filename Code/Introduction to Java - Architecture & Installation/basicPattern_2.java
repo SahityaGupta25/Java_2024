e5c3c31095b4378fd851d6e3bfb52641fc186de0
Sahitya Gupta <99082422+SahityaGupta25@users.noreply.github.com>
2025-09-22 22:06:57 +0530

    Create basicPattern_2.java

---
+++ Code/Introduction to Java - Architecture & Installation/basicPattern_2.java
@@ -0,0 +1,37 @@
+/*
+1111 
+2222 
+3333 
+4444 
+*/
+
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        // System.out.println("Enter rows in digit");
+        // int x = s.nextInt();
+        // System.out.println("Enter value of 'n'");
+        // int n = s.nextInt();
+    
+        int i = 1; // row
+        while (i<=4){
+            int j = 1; // col
+            while(j<=4){
+            
+             
+                System.out.print(i);
+               j++;
+            }
+            System.out.println(" ");
+            i++;
+            
+            
+        }
+        
+       
+
+        
+        
+    }
+}
