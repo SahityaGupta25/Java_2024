80cbcf0a058e0a53175ad9818f1c77a6b1ec769c
Sahitya Gupta <99082422+SahityaGupta25@users.noreply.github.com>
2025-09-22 12:16:38 +0530

    Created FindAPowerOfNumber.java

---
+++ Code/Introduction to Java - Architecture & Installation/FindAPowerOfNumber.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Solution {
+    
+    public static void main(String[] args) {
+        // Write your code here
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int x = s.nextInt();
+        int result = (int)Math.pow(n,x);
+        System.out.println(result);
+        
+            }
+}
