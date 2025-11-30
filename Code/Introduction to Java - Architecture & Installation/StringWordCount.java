9534babe046da0b1edf0cf9f929aca35df55a03f
Sahitya Gupta <99082422+SahityaGupta25@users.noreply.github.com>
2025-11-30 17:04:27 +0530

    Correct word counting implementation in StringWordCount
    Fix word counting logic to iterate through the string correctly.

--- Code/Introduction to Java - Architecture & Installation/StringWordCount.java
+++ Code/Introduction to Java - Architecture & Installation/StringWordCount.java
@@ -33,3 +33,25 @@ public class Solution {
 
 
 */
+
+import java.util.Scanner;
+public class Solution {
+
+	public static int countWords(String str) {	
+		int count =1;
+		for(int i=0; i<str.length();i++){
+			if(str.charAt(i)==' '){
+				count++;
+			}
+		}
+		return count;
+	}
+
+	public static void main(String[] args){
+		Scanner s = new Scanner(System.in);
+		String str = s.nextLine();
+        System.out.println(countWords(str));
+		
+	}
+
+}
